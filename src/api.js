c75452176b1317d1f8adea96a48856db37363158
Glushkov Daniil <super.stars.2020@bk.ru>
2025-11-29 16:16:46 +0300

    часть работы

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 export function getProducts({ pagination, filter, sorter }) {
   let paginatedUrl = `http://localhost:3001/products?_page=${pagination.page}`;
-  paginatedUrl += "&count_gte=10"; // TODO замените на данные из поля filter
-
-  // TODO добавить обработку сортировки из поля sorter
+  
+  // Обработка фильтрации
+  if (filter && filter.count !== undefined) {
+    paginatedUrl += `&count_gte=${filter.count}`;
+  }
+  
+  // Обработка сортировки
+  if (sorter && sorter.field) {
+    const order = sorter.order === 'descend' ? 'desc' : 'asc';
+    paginatedUrl += `&_sort=${sorter.field}&_order=${order}`;
+  }
 
   return fetch(paginatedUrl).then((response) => {
     if (!response.ok) {
@@ -12,3 +20,4 @@ export function getProducts({ pagination, filter, sorter }) {
     return response.json();
   });
 }
+
