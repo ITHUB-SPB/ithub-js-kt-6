232b3f4d68d21d49b67309593670ca34db9028d6
daslef <34357049+daslef@users.noreply.github.com>
2025-11-29 13:54:06 +0300

    Teacher (#5)
    * starter
    
    * fix (lint, format)
    
    ---------
    
    Co-authored-by: Трофимов Алексей <a.trofimov@domain.local>

--- src/dom.js
+++ src/dom.js
@@ -1,47 +1,52 @@
 export function renderTable(productsData) {
-    const tableBodyElement = document.querySelector("#products > tbody");
-    tableBodyElement.innerHTML = "";
+  const tableBodyElement = document.querySelector("#products > tbody");
+  tableBodyElement.innerHTML = "";
 
-    tableBodyElement.append(
-        ...productsData.map((productRecord) => {
-            const rowElement = document.createElement("tr");
-            rowElement.innerHTML = `
+  tableBodyElement.append(
+    ...productsData.map((productRecord) => {
+      const rowElement = document.createElement("tr");
+      rowElement.innerHTML = `
                 <td>${productRecord.title}</td>
                 <td>${productRecord.price}</td>
                 <td>${productRecord.count}</td>
+
+                <!-- TODO заменить массив на средний балл и количество, например 3.4 (8) -->
                 <td>${JSON.stringify(productRecord.marks)}</td>
             `;
-            return rowElement;
-        })
-    );
+      return rowElement;
+    }),
+  );
 }
 
 export function renderPagination({ current, total }) {
-    const pages = Array.from({ length: total }, (_, i) => i + 1)
+  const pages = Array.from({ length: total }, (_, i) => i + 1);
 
-    const paginationElement = document.querySelector("#pagination");
-    paginationElement.innerHTML = ''
+  const paginationElement = document.querySelector("#pagination");
+  paginationElement.innerHTML = "";
 
-    paginationElement.append(
-        ...pages.map((pageNumber) => {
-            const buttonElement = document.createElement('button')
-            buttonElement.textContent = String(pageNumber)
-            buttonElement.className = `uk-btn uk-btn-sm ${pageNumber == current ? 'uk-btn-ghosted' : 'uk-btn-primary'}`
-            return buttonElement
-        })
-    )
+  paginationElement.append(
+    ...pages.map((pageNumber) => {
+      const buttonElement = document.createElement("button");
+      buttonElement.textContent = String(pageNumber);
+      buttonElement.className = `uk-btn uk-btn-sm ${pageNumber == current ? "uk-btn-ghosted" : "uk-btn-primary"}`;
+      return buttonElement;
+    }),
+  );
 }
 
 export function showNotification(message, type, timeout = 5000) {
-    const iconTemplates = {
-        error: "<span class='flex-none mr-2'><uk-icon icon='circle-slash'></uk-icon></span>",
-        pending: "<span class='flex-none mr-2'><uk-icon icon='loader-pinwheel'></uk-icon></span>",
-        success: "<span class='flex-none mr-2'><uk-icon icon='smile'></uk-icon></span>"
-    }
+  const iconTemplates = {
+    error:
+      "<span class='flex-none mr-2'><uk-icon icon='circle-slash'></uk-icon></span>",
+    pending:
+      "<span class='flex-none mr-2'><uk-icon icon='loader-pinwheel'></uk-icon></span>",
+    success:
+      "<span class='flex-none mr-2'><uk-icon icon='smile'></uk-icon></span>",
+  };
 
-    UIkit.notification({
-        message: `<div class='flex items-center'>${iconTemplates[type] ?? ""}${message}</div>`,
-        pos: "top",
-        timeout,
-    });
+  UIkit.notification({
+    message: `<div class='flex items-center'>${iconTemplates[type] ?? ""}${message}</div>`,
+    pos: "top",
+    timeout,
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,51 @@
-import './global.css'
+import "./global.css";
 import "franken-ui/js/core.iife";
 import "franken-ui/js/icon.iife";
 
 import { getProducts } from "./api.js";
 import { renderTable, renderPagination, showNotification } from "./dom.js";
 
-
-function render(params) {
-  getProducts(params)
+function render({ pagination, filter, sorter }) {
+  getProducts({ pagination, filter, sorter })
     .then((data) => {
       renderTable(data.data);
-      renderPagination({ current: params.paginate.page, total: data.pages });
+      renderPagination({ current: pagination.page, total: data.pages });
     })
     .catch((error) => {
       showNotification(error.message, "error");
     });
 }
 
-
 document.querySelector("#pagination").addEventListener("click", (event) => {
-  // TODO
+  // TODO посмотрите на event.target, найдите в нем номер страницы
+  // TODO вызовите рендер со значением pagination: { page: <номер_страницы> }
 });
 
+document
+  .querySelector("кнопка-фильтра-по-цене")
+  ?.addEventListener("click", () => {
+    // TODO вызвать рендер с соответствующими пагинацией и фильтром
+  });
+
+document.querySelector("#filter-count").addEventListener("click", () => {
+  render({ pagination: { page: 1 }, filter: "count_gte=10" });
+});
+
+document
+  .querySelector("кнопка-сброса-фильтра")
+  ?.addEventListener("click", () => {
+    // TODO вызвать рендер с параметрами по умолчанию (без фильтров)
+  });
+
+document.querySelector("кнопка-сортировки")?.addEventListener("click", () => {
+  // TODO вызвать рендер с соответствующим параметром sorter
+});
+
+document.querySelector("#sort-disable")?.addEventListener("click", () => {
+  render({ pagination: { page: 1 } });
+  // TODO (со звездочкой) сохранять фильтры
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-  // TODO
+  render({ pagination: { page: 1 } });
 });
