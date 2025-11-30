9037a2f6d10e2fe29cc56d16f7c41dca4a9e62a1
1nterFekT <lavamishals@gmail.com>
2025-11-30 03:43:30 +0300

    added simulated delay from backend

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 export function getProducts(queryString) {
     let url = `http://localhost:3001/products?${queryString}`;
+    const delay = Math.floor(Math.random() * (5000 - 500 + 1)) + 500
 
-    return fetch(url).then((response) => {
-        if (!response.ok) {
-            throw new Error("Response status is not OK");
-        }
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Response status is not OK");
+            }
 
-        return response.json();
-    });
+            return response.json();
+        })
+        .then((data) => {
+            return new Promise((resolve) => {
+                setTimeout(() => resolve(data), delay) // TODO: Реализовать крутящийся спиннер во время ожидания ответа
+            })
+        });
 }
