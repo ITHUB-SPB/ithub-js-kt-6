be500b554578a4b6a3e766570c0f1d97e3894f7f
SD N <yaromirgames60@gmail.com>
2025-12-03 13:59:32 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,42 @@
 export function getProducts({ pagination, filter, sorter }) {
-  let paginatedUrl = `http://localhost:3001/products?_page=${pagination.page}`;
-  paginatedUrl += "&count_gte=10"; // TODO замените на данные из поля filter
 
-  // TODO добавить обработку сортировки из поля sorter
+  let url = http://localhost:3001/products?_page=${pagination.page}&_limit=10;
+  
 
-  return fetch(paginatedUrl).then((response) => {
-    if (!response.ok) {
-      throw new Error("Response status is not OK");
-    }
+  if (filter && filter !== "null") {
 
-    return response.json();
-  });
+    url += &${filter};
+  } else {
+
+    url += '&count_gte=10';
+  }
+  
+
+  if (sorter && sorter.field && sorter.order) {
+    url += &_sort=${sorter.field}&_order=${sorter.order};
+  }
+
+  console.log('Fetching from URL:', url); 
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(HTTP error! status: ${response.status});
+      }
+      
+
+      return Promise.all([
+        response.json(),
+        Promise.resolve(response.headers.get('X-Total-Count') || 0)
+      ]);
+    })
+    .then(([data, totalCount]) => {
+
+      const totalPages = Math.ceil(totalCount / 10);
+      
+      return {
+        data: data,
+        pages: totalPages
+      };
+    });
 }
